49bbda6bf375b99a6ec88b617f6c42f8aa3cbf26
Margarida Martelo <margmartelo@gmail.com>
2020-10-29 20:08:25 +0000

    Passing the first test

--- design_strategies_1/lib/remembering_names.rb
+++ design_strategies_1/lib/remembering_names.rb
@@ -1,2 +1,3 @@
 def adding_names(name)
+  "Name stored!"
 end
--- design_strategies_1/spec/remembering_names_spec.rb
+++ design_strategies_1/spec/remembering_names_spec.rb
@@ -1,6 +1,7 @@
 # As a student
 # So that I can remember people's names
 # I want an IRB application that stores the names of people I meet
+require 'remembering_names.rb'
 
 describe "the adding_names method" do
   it "stores the names of the people we met" do
